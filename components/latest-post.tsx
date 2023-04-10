cdd8c0a08e71e3adeb57790cdd7b05c266381f05
Januar Fonti <januar@fonti.dev>
2023-04-11 01:49:43 +0700

    fix: latest post link

--- components/latest-post.tsx
+++ components/latest-post.tsx
@@ -18,33 +18,31 @@ function LatestPosts() {
       {posts?.length ? (
         <section className="grid gap-10 sm:grid-cols-2 md:max-w-[52rem]">
           {posts.map((post, index) => (
-            <article
-              key={post._id}
-              className="group relative flex flex-col space-y-2"
-            >
-              {post.image && (
-                <Image
-                  src={post.image}
-                  alt={post.title}
-                  width={804}
-                  height={452}
-                  className="rounded-md border border-slate-200 bg-slate-200 transition-colors group-hover:border-slate-900"
-                  priority={index <= 1}
-                />
-              )}
-              <h2 className="text-xl font-extrabold md:text-2xl">{post.title}</h2>
-              {post.description && (
-                <p className="text-slate-600">{post.description}</p>
-              )}
-              {post.date && (
-                <p className="text-sm text-slate-600">
-                  {formatDate(post.date)}
-                </p>
-              )}
-              <Link href={post.slug} className="umami--click--blog-detail-link absolute inset-0">
-                <span className="sr-only">View Article</span>
-              </Link>
-            </article>
+            <Link key={post._id} href={post.slug} className="umami--click--blog-detail-link">
+              <article
+                className="group relative flex flex-col space-y-2"
+              >
+                {post.image && (
+                  <Image
+                    src={post.image}
+                    alt={post.title}
+                    width={804}
+                    height={452}
+                    className="rounded-md border border-slate-200 bg-slate-200 transition-colors group-hover:border-slate-900"
+                    priority={index <= 1}
+                  />
+                )}
+                <h2 className="text-xl font-extrabold md:text-2xl">{post.title}</h2>
+                {post.description && (
+                  <p className="text-slate-600">{post.description}</p>
+                )}
+                {post.date && (
+                  <p className="text-sm text-slate-600">
+                    {formatDate(post.date)}
+                  </p>
+                )}
+              </article>
+            </Link>
           ))}
         </section>
       ) : (
