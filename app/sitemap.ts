4a2b2bb7fb341c2269317a7996ea20f75fa8cc82
Januar Fonti <januar@fonti.dev>
2023-06-03 21:53:43 +0700

    refactor(sitemap.ts): remove redundant path segments from generated URLs

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,12 +2,12 @@ import { allPosts, allPlaygrounds } from 'contentlayer/generated';
 
 export default async function sitemap() {
   const blogs = allPosts.map((post) => ({
-    url: `https://fonti.dev/blog/${post.slug}`,
+    url: `https://fonti.dev${post.slug}`,
     lastModified: post.date,
   }));
   
   const playgrounds = allPlaygrounds.map((playground) => ({
-    url: `https://fonti.dev/playgrounds/${playground.slug}`,
+    url: `https://fonti.dev${playground.slug}`,
     lastModified: playground.date,
   }));
 
