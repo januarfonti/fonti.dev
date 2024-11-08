6f56b92bb0d0f37ffd7300be19dc938da648138f
Januar Fonti <januar@fonti.dev>
2024-11-08 22:18:20 +0700

    fix(next.config.ts): remove unused image domain configuration for placehold.co and picsum.photos to declutter config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import { withContentCollections } from "@content-collections/next";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    // domains: ["placehold.co", "picsum.photos"],
-    // dangerouslyAllowSVG: true,
-    // contentDispositionType: "attachment",
     remotePatterns: [
       {
         hostname: "placehold.co",
